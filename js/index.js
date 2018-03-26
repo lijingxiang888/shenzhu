db2277389a72d0a3afb13ec1e263112a7fec8933
lijingxiang888 <924415666@qq.com>
2018-03-26 14:19:35 +0800

    完善“模拟placeholder”

--- js/index.js
+++ js/index.js
@@ -240,10 +240,44 @@ $(function () {
 
 $(function () {
     var $input = $('.sign-in input');
-
+    //十六进制颜色值的正则表达式
+    var reg = /^#([0-9a-fA-f]{3}|[0-9a-fA-f]{6})$/;
+    /*RGB颜色转换为16进制*/
+    String.prototype.colorHex = function () {
+        var that = this;
+        if (/^(rgb|RGB)/.test(that)) {
+            var aColor = that.replace(/(?:\(|\)|rgb|RGB)*/g, "").split(",");
+            var strHex = "#";
+            for (var i = 0; i < aColor.length; i++) {
+                var hex = Number(aColor[i]).toString(16);
+                if (hex === "0") {
+                    hex += hex;
+                }
+                strHex += hex;
+            }
+            if (strHex.length !== 7) {
+                strHex = that;
+            }
+            return strHex;
+        } else if (reg.test(that)) {
+            var aNum = that.replace(/#/, "").split("");
+            if (aNum.length === 6) {
+                return that;
+            } else if (aNum.length === 3) {
+                var numHex = "#";
+                for (var i = 0; i < aNum.length; i += 1) {
+                    numHex += (aNum[i] + aNum[i]);
+                }
+                return numHex;
+            }
+        } else {
+            return that;
+        }
+    };
     $input.focus(function () {
-        var $Color = $(this).css('color');
-        if ('rgb(152, 152, 152)' === $Color){
+        var $color = $(this).css('color');
+        var $colorHex = $color.colorHex().toString();//string，但ie8及其以下是对象数据类型，需要toString转换为字符串
+        if ('#989898' === $colorHex){
             $(this).val('');
             $(this).css({'color':'#000'})
         }
--- js/myplaceholder.js
+++
@@ -1,3 +0,0 @@
-// 模拟placeholder
-//<input tabindex="4" type="text" id="placeh" value="电视盒子" style="color:#989898">
-
